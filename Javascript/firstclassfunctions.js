0208905ea213b5ef9b7852dc3c02884ed55002c3
Sakshi Shah <67853646+Sakshi-Shah5@users.noreply.github.com>
2022-08-02 08:50:26 +0530

    Create firstclassfunctions.js

---
+++ Javascript/firstclassfunctions.js
@@ -0,0 +1,39 @@
+//first class functions
+
+//1. function is assigned as a value to a variable 
+//2. function is passed as a parameter to another function(callback function) :
+
+var sayHello = function(){
+    return "Hi, how are you?";
+}
+
+function greetings(name, helloMsg){
+ console.log(name + helloMsg());
+}
+
+greetings("Soham ", sayHello);
+
+//sayHello();
+
+
+
+//3. Higher Order functions
+
+function promotionCalculator(designation){
+    if(designation == "Manager"){
+        return function(experience){
+            console.log("Promotion for " + designation + " with experience " + experience + " is Senior Manager");
+        }
+    }
+    else{
+        return function(experience){
+            console.log("Promotion for " + designation + " with experience " + experience + " is based on experience");
+        }
+    }
+}
+
+var promotion = promotionCalculator("SDE");
+promotion(5);
+
+//OR we can can the function in this way too:
+//promotionCalculator("Manager")(5);
